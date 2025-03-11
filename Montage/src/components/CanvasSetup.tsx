4964fbafc4f8be11b39b4f15f3938e0525f57d37
Nirali-Darji <nirali@hexacoder.com>
2025-03-11 12:36:16 +0530

    model stored at configuratorStore

--- Montage/src/components/CanvasSetup.tsx
+++ Montage/src/components/CanvasSetup.tsx
@@ -5,60 +5,64 @@ import CanvasDropHandler from "../utils/CanvasDropHandler";
 import Model from "./Model";
 import { observer } from "mobx-react-lite";
 // import MultiSelectionControls from "../utils/MultiSelectionControls";
-import * as THREE from 'three'
+import * as THREE from "three";
 
 const CanvasSetup = observer(() => {
-    // const { camera } = useThree();
-  
-    // // Update camera look-at when view mode changes
-    // useEffect(() => {
-    //   camera.lookAt(0, 0, 0);
-    // }, [store.viewMode, camera]);
-  
-    return (
-      <>
-        <CanvasDropHandler/>
-        <CameraSetUp />
-        <ambientLight intensity={1} />
-  <directionalLight position={[10, 10, 5]} intensity={1} />
-  <pointLight position={[0, 10, 0]} intensity={0.5} />
-  <mesh rotation={[-Math.PI / 2, 0, 0] } position={[0, -2, 0]}>
-    <planeGeometry args={[100, 100]}  />
-    <meshBasicMaterial   color={new THREE.Color(0xffffff)} />
-  </mesh>
-  
-        {/* Use restricted controls for 2D view */}
-        {store.viewMode === '2D' ? (
-          <OrbitControls
-            makeDefault
-            enableRotate={false}
-            minPolarAngle={Math.PI / 2}
-            maxPolarAngle={Math.PI / 2}
-          />
-        ) : (
-          <OrbitControls makeDefault />
-        )}
-  
-        {/* Grid helper for 2D view */}
-        {store.viewMode === '2D' && (
-          <gridHelper args={[100, 100, 0xf0f0f0, 0xf0f0f0]} position={[0, -2, 0]}  />
-        )}
-  
-        {/* Load all models */}
-        {store.models.map((model) => (
-          <Model
-            key={model.id}
-            id={model.id}
-            url={model.url}
-            position={model.position}
-          />
-        ))}
-  
-        {/* Add transform controls for multi-selection */}
-        {/* <MultiSelectionControls /> */}
-      </>
-    );
-  });
-  
+  // const { camera } = useThree();
 
-export default CanvasSetup
+  // // Update camera look-at when view mode changes
+  // useEffect(() => {
+  //   camera.lookAt(0, 0, 0);
+  // }, [store.viewMode, camera]);
+
+  return (
+    <>
+      <CanvasDropHandler />
+      <CameraSetUp />
+      <ambientLight intensity={1.5} />
+      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <directionalLight position={[-10, -10, -5]} intensity={1} />
+      <directionalLight position={[10, 10, -5]} intensity={1} />
+      {/* <pointLight position={[0, 10, 0]} intensity={1} /> */}
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]}>
+        <planeGeometry args={[100, 100]} />
+        <meshBasicMaterial color={new THREE.Color(0xffffff)} />
+      </mesh>
+
+      {/* Use restricted controls for 2D view */}
+      {store.viewMode === "2D" ? (
+        <OrbitControls
+          makeDefault
+          enableRotate={false}
+          minPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI / 2}
+        />
+      ) : (
+        <OrbitControls makeDefault />
+      )}
+
+      {/* Grid helper for 2D view */}
+      {store.viewMode === "2D" && (
+        <gridHelper
+          args={[100, 100, 0xf0f0f0, 0xf0f0f0]}
+          position={[0, -2, 0]}
+        />
+      )}
+
+      {/* Load all models */}
+      {store.models.map((model) => (
+        <Model
+          key={model.id}
+          id={model.id}
+          url={model.url}
+          position={model.position}
+        />
+      ))}
+
+      {/* Add transform controls for multi-selection */}
+      {/* <MultiSelectionControls /> */}
+    </>
+  );
+});
+
+export default CanvasSetup;
