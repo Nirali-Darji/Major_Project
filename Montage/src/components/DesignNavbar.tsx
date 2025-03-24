3b012faeb5c174db07c8205c151974431df8ef3c
Krishna-Ankoliya19 <krishna@hexacoder.com>
2025-03-24 10:41:30 +0530

    logo added

--- Montage/src/components/DesignNavbar.tsx
+++ Montage/src/components/DesignNavbar.tsx
@@ -18,7 +18,8 @@ function DesignNavbar() {
   return (
     <div className="flex justify-between w-full items-center bg-gray-100 border-b-2 border-[#DCDCDC] p-3 z-10">
       <div className="flex items-center gap-10">
-        <h2 className="text-2xl">Montage</h2>
+        {/* <h2 className="text-2xl">Montage</h2> */}
+        <img src="http://montage-prod.s3-website.ap-south-1.amazonaws.com/assets/logo/logo-icon.svg" alt="" />
         <button className="text-2xl" onClick={() => navigate("/portfolio")}>
           {portfolioName}
         </button>
